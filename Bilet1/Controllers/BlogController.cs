536bd4f70d4f10c74c2d31cded57f68523bb2fc2
abdulazimovaNigar <nigarna-mpa201@code.edu.az>
2026-01-15 14:07:38 +0400

    first commit

---
+++ Bilet1/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bilet1.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
